ba4997ff5325a446d5fbee58dc4bd1c48898dce1
Serebro1 <OtcheskovSemen@yandex.ru>
2024-12-09 09:35:18 +0300

    Video&ImgPack with layouts

--- DetectorCLR.py
+++ DetectorCLR.py
@@ -18,7 +18,7 @@ def cli_argument_parser():
                         help='Path to a dir of layout',
                         type=str,
                         dest='dir_layout_path',
-                        default=None)
+                        default="../DLMini/layout/mov03478.txt")
     parser.add_argument('--model',
                         help='Path to a model',
                         type=str,
@@ -60,7 +60,7 @@ def image_show(image_path):
     cv.destroyAllWindows()
 
 
-def video_processing(video_path, model_path, layout_dir):
+def video_processing(video_path, model_path, layout_file):
     if video_path is not None:
         capt = cv.VideoCapture(video_path)
 
@@ -68,10 +68,23 @@ def video_processing(video_path, model_path, layout_dir):
         raise ValueError('Path of the video file is incorrect '
                          'or camera is unavailable')
     #возможная обработка видео
+    total_frames = int(capt.get(cv.CAP_PROP_FRAME_COUNT))
+
+    frame_layouts = get_layouts(layout_file, total_frames)
+
+    numFrame = 0
     while capt.isOpened():
         ret, frame = capt.read()
-        if ret:    
+        if ret:
+            boxes = frame_layouts.get(numFrame, [])
+            for box in boxes:
+                label, x1, y1, x2, y2 = box
+                # Рисуем прямоугольник
+                cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Зеленый прямоугольник, толщина 2
+                # Подписываем класс
+                cv.putText(frame, label, (x1+5, y1 + 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
             cv.imshow("Video", frame)
+            numFrame+=1
             # Press Q on the keyboard to exit
             if cv.waitKey(25) & 0xFF == ord('q'):
                 break
@@ -81,18 +94,60 @@ def video_processing(video_path, model_path, layout_dir):
     capt.release()
     cv.destroyAllWindows()
 
-def image_pack_processing(image_dir, model_path, layout_dir):
+def image_pack_processing(image_dir, model_path, layout_file):
     image_files = [file for file in os.listdir(image_dir) if file.lower().endswith((".jpg", ".jpeg", ".png"))]
     num_of_images = len(image_files)
+    print(f"Найдено изображений: {num_of_images}")
     if num_of_images == 0:
         raise ValueError("No images found in the specified directory.")
     # возможная обработка изображений
+    
+    frame_layouts = get_layouts(layout_file, num_of_images)
+
+    numFrame = 0
     for img in image_files:
+
         image = cv.imread(image_dir + img)
-        #
+        
+        if image is None:
+            print(f"Изображение {image_dir + img} не найдено. Пропускаем.")
+            continue
+        
+        boxes = frame_layouts.get(numFrame, [])
+        for box in boxes:
+            label, x1, y1, x2, y2 = box
+            # Рисуем прямоугольник
+            cv.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Зеленый прямоугольник, толщина 2
+            # Подписываем класс
+            cv.putText(image, label, (x1+5, y1 + 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         cv.imshow(f'Image pack', image) 
+
+        numFrame+=1
         if cv.waitKey(25) & 0xFF == ord('q'):
             break
+
+def get_layouts(layout_file, num_images):
+    frame_layouts = {}
+    with open(layout_file, 'r') as file:
+        layouts = file.readlines()
+    
+    for line in layouts:
+        parts = line.strip().split()
+        frame_idx = int(parts[0])
+        
+        # Игнорируем строки с кадрами, превышающими количество изображений
+        if frame_idx == num_images:
+            break
+        
+        label = parts[1]
+        x1, y1 = int(parts[2]), int(parts[3])
+        x2, y2 = int(parts[4]), int(parts[5])
+        
+        if frame_idx not in frame_layouts:
+            frame_layouts[frame_idx] = []
+        frame_layouts[frame_idx].append((label, x1, y1, x2, y2))
+    return frame_layouts
+
 
 def main():
     args = cli_argument_parser()
